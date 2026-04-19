4f52dcfe22542a013e0b35dac6f4980ccfbb5dd3
Ankit Raj <123945458+Ankit95040@users.noreply.github.com>
2026-04-19 12:20:54 +0530

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp=new int[m][n];
+        for(int[] ele:dp){
+            Arrays.fill(ele,-1);
+        }
+        return helper(m-1,n-1,dp);
+    }
+    public int helper(int r,int c,int[][] dp){
+        if(r==0 && c==0){
+            return 1;
+        }
+        if(r<0 || c<0){
+            return 0;
+        }
+        if(dp[r][c]!=-1){
+            return dp[r][c];
+        }
+        int up=helper(r-1,c,dp);
+        int left=helper(r,c-1,dp);
+        return dp[r][c]=up+left;
+    }
+}
