c37ba81fe032443a430767fe0a1a0b9d3d36b764
Ankit Raj <123945458+Ankit95040@users.noreply.github.com>
2026-05-05 19:41:34 +0530

    Add: subarray-product-less-than-k - submission-1

---
+++ Data Structures & Algorithms/subarray-product-less-than-k/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+
+        if (k <= 1) return 0;
+
+        int l = 0, r = 0;
+        int product = 1;
+        int count = 0;
+
+        while (r < nums.length) {
+            product *= nums[r];
+
+            while (product >= k) {
+                product /= nums[l];
+                l++;
+            }
+
+            count += r - l + 1;
+            r++;
+        }
+
+        return count;
+    }
+}
