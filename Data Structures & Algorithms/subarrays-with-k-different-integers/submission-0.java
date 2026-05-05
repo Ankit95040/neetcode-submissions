7eb23fc45760168ef8723df62166efa75a737f47
Ankit Raj <123945458+Ankit95040@users.noreply.github.com>
2026-05-06 01:19:53 +0530

    Add: subarrays-with-k-different-integers - submission-0

---
+++ Data Structures & Algorithms/subarrays-with-k-different-integers/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+
+    private int atMost(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int l = 0, count = 0;
+
+        for (int r = 0; r < nums.length; r++) {
+
+            map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
+
+            while (map.size() > k) {
+                map.put(nums[l], map.get(nums[l]) - 1);
+                if (map.get(nums[l]) == 0) {
+                    map.remove(nums[l]);
+                }
+                l++;
+            }
+
+            count += r - l + 1;
+        }
+
+        return count;
+    }
+}
